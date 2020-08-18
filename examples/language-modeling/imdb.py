aec3f1cf0fc2056e071a51ed54c6be6038e53a93
muhammadfahid <muhammad.fahid@arbisoft.com>
2020-08-18 13:46:48 +0500

    [sentiment] model changed to tensorflow

--- examples/language-modeling/imdb.py
+++ examples/language-modeling/imdb.py
@@ -2,15 +2,17 @@
 
 import pandas as pd
 
-from transformers import RobertaTokenizer, RobertaForSequenceClassification
+from transformers import RobertaTokenizer, TFRobertaForSequenceClassification
 
 # df_train = pd.read_csv("urdu_imdb_review_train.csv")
 # df_test = pd.read_csv("urdu_imdb_review_test.csv")
 
 
 tokenizer = RobertaTokenizer.from_pretrained("roberta-urdu-small")
-model = RobertaForSequenceClassification.from_pretrained("roberta-urdu-small")
+model = TFRobertaForSequenceClassification.from_pretrained("roberta-urdu-small", from_pt=True)
+
 print(model.summary())
+
 
 
 
