db8c73b2e3697cfb5d8056c8f528081f591a1cee
muhammadfahid <muhammad.fahid@arbisoft.com>
2020-08-18 16:51:00 +0500

    [val] validation data added

--- examples/language-modeling/imdb.py
+++ examples/language-modeling/imdb.py
@@ -16,25 +16,35 @@ model.compile(optimizer=tf.keras.optimizers.Adam(lr=0.0001), loss="binary_crosse
               metrics=["accuracy"])
 
 
-input_ids = []
-attention_masks = []
+train_input_ids = []
+train_attention_masks = []
+test_input_ids = []
+test_attention_masks = []
 
 for text in df_train["review"].values:
     encoded = tokenizer.encode_plus(text)
-    input_ids.append(encoded["input_ids"])
-    attention_masks.append(encoded["attention_mask"])
+    train_input_ids.append(encoded["input_ids"])
+    train_attention_masks.append(encoded["attention_mask"])
+
+for text in df_test["review"].values:
+    encoded = tokenizer.encode_plus(text)
+    test_input_ids.append(encoded["input_ids"])
+    test_attention_masks.append(encoded["attention_mask"])
 
 label_to_int = {"positive": 1, "negative": 0}
 train_labels = [label_to_int[label] for label in df_train["sentiment"].values]
+test_labels = [label_to_int[label] for label in df_test["sentiment"].values]
 train_labels = tf.keras.utils.to_categorical(train_labels)
+test_labels = tf.keras.utils.to_categorical(test_labels)
 
-input_ids = tf.keras.preprocessing.sequence.pad_sequences(input_ids, maxlen=256, padding="post", value=1)
-attention_masks = tf.keras.preprocessing.sequence.pad_sequences(attention_masks, maxlen=256, padding="post", value=0)
-
-
-model.fit(x=[input_ids, attention_masks], y=train_labels, epochs=5, batch_size=16)
+train_input_ids = tf.keras.preprocessing.sequence.pad_sequences(train_input_ids, maxlen=256, padding="post", value=1)
+train_attention_masks = tf.keras.preprocessing.sequence.pad_sequences(train_attention_masks, maxlen=256, padding="post", value=0)
 
 
+model.fit(x=[train_input_ids, train_attention_masks], y=train_labels, epochs=5, batch_size=16,
+          validation_data=([test_input_ids, test_attention_masks], test_labels))
+
+model.save("tf-sentiment-model.h5")
 
 
 
