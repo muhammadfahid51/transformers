080cb9a678442da49e1299dfb1078312c9bac9b9
muhammadfahid <muhammad.fahid@arbisoft.com>
2020-08-18 14:25:34 +0500

    [sentiment] load in TFRobertaModel

--- examples/language-modeling/imdb.py
+++ examples/language-modeling/imdb.py
@@ -2,14 +2,17 @@
 
 import pandas as pd
 
-from transformers import RobertaTokenizer, TFRobertaForSequenceClassification
+from transformers import RobertaTokenizer, TFRobertaForSequenceClassification, TFRobertaModel
 
 # df_train = pd.read_csv("urdu_imdb_review_train.csv")
 # df_test = pd.read_csv("urdu_imdb_review_test.csv")
 
 
 tokenizer = RobertaTokenizer.from_pretrained("roberta-urdu-small")
+roberta = TFRobertaModel.from_pretrained("roberta-urdu-small", from_pt=True)
+roberta.save_pretrained("tf-roberta-urdu-small")
 model = TFRobertaForSequenceClassification.from_pretrained("roberta-urdu-small", from_pt=True)
+
 
 print(model.summary())
 
